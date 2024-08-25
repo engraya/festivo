b640132fa124d596e2a4d9fc6e7b939c0fd35676
Engraya <ahmadyakubuaya@gmail.com>
2024-08-25 15:24:29 +0100

    Added some changes to Configs

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-
+ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware({
+  // @ts-ignore
   publicRoutes: [
     '/',
     '/events/:id',
@@ -16,12 +15,6 @@ export default clerkMiddleware({
   ]
 });
  
-
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
